a596ca6056d76cc7ee0de066bff234bbc6628274
나플 <jindsjds@kakao.com>
2025-08-11 05:32:43 +0000

    release 2.2

--- calc_api.py
+++ calc_api.py
@@ -1,6 +1,6 @@
 ##### [ 계산기 API ]
 ##### CalculatorAPI in python by Napple7724 on August 5, 2025
-##### Version : release 1.1.1
+##### Version : release 1.2
 #################################################################
 ## 실행하기 전 "README.md"와 "LICENSE.md"를 읽어주세요.
 ## Please read "README.md" and "LICENSE.md" before running.
@@ -11,7 +11,9 @@ def calc_api(list_num_api) :
     from random import uniform
     while True :
         sleep(uniform(0.03, 0.1))
-        str_sign = input("부호(+, -, *, /, ^)를 입력하세요.\n  : ")
+        print("부호(+, -, *, /, ^)를 입력하세요.")
+        sleep(uniform(0.01, 0.3))
+        str_sign = input("  : ")
         int_a = list_num_api[0]
         if str_sign == "+" :
             int_a = sum(list_num_api)
@@ -26,13 +28,19 @@ def calc_api(list_num_api) :
                 for sys_int_r1 in range(1, len(list_num_api)) :
                     int_a /= list_num_api[sys_int_r1]
             except ZeroDivisionError :
-                print("\n\n오류! : 0으로 나눌 수 없습니다.")
+                print("\n")
+                sleep(uniform(0.01, 0.3))
+                print("오류! : 0으로 나눌 수 없습니다.")
                 continue
         elif str_sign == "^" :
             for sys_int_r1 in range(1, len(list_num_api)) :
                 int_a **= list_num_api[sys_int_r1]
         else :
-            print("\n\n오류! : 정해진 부호 중 하나를 입력하세요.")
+            print("\n")
+            sleep(uniform(0.01, 0.3))
+            print("오류! : 정해진 부호 중 하나를 입력하세요.")
             continue
         break
+    print("\n")
+    sleep(uniform(0.01, 0.3))
     return int_a
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 ##### [ 계산기 ]
 ##### Calculator in python by Napple7724 on August 5, 2025
-##### Version : release 2.1.2
+##### Version : release 2.2
 #################################################################
 ## 실행하기 전 "README.md"와 "LICENSE.md"를 읽어주세요.
 ## Please read "README.md" and "LICENSE.md" before running.
@@ -16,42 +16,80 @@ print("Loading...")
 sleep(uniform(0.3, 0.5))
 while True :
     try :
-        print("\n\n[ 계산기 ]\nCalculator in python by Napple7724 on August 5, 2025\nVersion : release 2.1.2\n")
+        print("\n")
+        sleep(uniform(0.01, 0.3))
+        print("[ 계산기 ]")
+        sleep(uniform(0.01, 0.3))
+        print("Calculator in python by Napple7724 on August 5, 2025")
+        sleep(uniform(0.01, 0.3))
+        print("Version : release 2.2")
+        sleep(uniform(0.01, 0.3))
+        print("실행중에는 [^C] 키를 눌러 종료할 수 있습니다.")
+        sleep(uniform(0.01, 0.3))
+        print()
         sleep(uniform(0.03, 0.1))
         while True :
             try :
-                list_num = list(map(int, input("숫자를 공백으로 나눠 입력하세요.\n  : ").split()))
+                print("숫자를 공백으로 나눠 입력하세요.")
+                sleep(uniform(0.01, 0.3))
+                list_num = list(map(int, input("  : ").split()))
                 if len(list_num) >= 2 :
                     break
                 else :
-                    print("\n\n오류! : 항목을 2개 이상 입력하세요.")
+                    print("\n")
+                    sleep(uniform(0.01, 0.3))
+                    print("오류! : 항목을 2개 이상 입력하세요.")
             except ValueError :
-                print("\n\n오류! : 숫자를 입력하세요.")
+                print("\n")
+                sleep(uniform(0.01, 0.3))
+                print("오류! : 숫자를 입력하세요.")
             sleep(uniform(0.03, 0.1))
         print("\n")
+        sleep(uniform(0.01, 0.3))
         print(f"\n\n :: {calc_api(list_num)}")
     except KeyboardInterrupt :
-        print("\n\n종료합니다.")
+        print("\n")
+        sleep(uniform(0.01, 0.3))
+        print("종료합니다.")
         break
     except :
-        print("\n\n오류! : 알 수 없는 오류가 발생했습니다.")
+        print("\n")
+        sleep(uniform(0.01, 0.3))
+        print("오류! : 알 수 없는 오류가 발생했습니다.")
+        break
     print("\n")
+    sleep(uniform(0.01, 0.3))
     try :
         while True :
             sleep(uniform(0.03, 0.1))
-            str_q = input("계속 하시겠습니까? (Y/N)\n  : ")
+            print("계속 하시겠습니까? (Y/N)")
+            sleep(uniform(0.01, 0.3))
+            str_q = input("  : ")
             if str_q == "Y" or str_q == "y" :
                 sleep(uniform(0.03, 0.1))
-                print("\n\n다시 시작합니다.")
+                print("\n\n")
+                sleep(uniform(0.01, 0.3))
+                print("다시 시작합니다.")
                 sleep(uniform(0.03, 0.1))
                 break
             if str_q == "N" or str_q == "n" :
                 sleep(uniform(0.03, 0.1))
-                print("\n\n종료합니다.")
+                print("\n")
+                sleep(uniform(0.01, 0.3))
+                print("종료합니다.")
                 sleep(uniform(0.03, 0.1))
                 sys_exit()
             else :
-                print("\n\n오류! : 정해진 문자 중 하나를 입력하세요.")
+                print("\n")
+                sleep(uniform(0.01, 0.3))
+                print("오류! : 정해진 문자 중 하나를 입력하세요.")
     except KeyboardInterrupt :
-        print("\n\n종료합니다.")
+        print("\n")
+        sleep(uniform(0.01, 0.3))
+        print("종료합니다.")
         break
+    # except :
+    #     print("\n")
+    #     sleep(uniform(0.01, 0.3))
+    #     print("오류! : 알 수 없는 오류가 발생했습니다.")
+    ## 해당 부분을 코드에 삽입할 경우 원인을 알 수 없는 오류 발생
